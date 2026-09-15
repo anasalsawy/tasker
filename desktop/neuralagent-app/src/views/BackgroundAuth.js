92e33c5cc1a022a67a0c1addba4f9e7c7a1eee41
anasalsawy <118839628+anasalsawy@users.noreply.github.com>
2026-09-16 00:34:55 +0300

    Use Tasker branding in background authentication

--- desktop/neuralagent-app/src/views/BackgroundAuth.js
+++ desktop/neuralagent-app/src/views/BackgroundAuth.js
@@ -36,7 +36,7 @@ export default function BackgroundAuth() {
   return (
     <Container>
       <InstructionBox>
-        Log in to any sites or apps you'd like <Highlight>NeuralAgent</Highlight> to control in the background. Close the window when you finish.<br />
+        Log in to any sites or apps you'd like <Highlight>Tasker</Highlight> to control in the background. Close the window when you finish.<br />
         <small style={{ opacity: 0.7 }}>
           These sessions are stored securely on your computer. You can always do this from App &gt; Background Mode Authentication.
         </small>
