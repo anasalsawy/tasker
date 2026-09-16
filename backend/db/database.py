722a488ef466339440ce01bcac5683e4031cb323
anasalsawy <118839628+anasalsawy@users.noreply.github.com>
2026-09-16 03:18:27 +0300

    Support local SQLite development database

--- backend/db/database.py
+++ backend/db/database.py
@@ -6,11 +6,21 @@ import os
 load_dotenv()
 
 
-DATABASE_URL = ('postgresql://' + os.getenv('DB_USERNAME') + ':' + os.getenv('DB_PASSWORD') +
-                '@' + os.getenv('DB_HOST') + ':' + os.getenv('DB_PORT') + '/' + os.getenv('DB_DATABASE')) \
-    if not os.getenv('DB_CONNECTION_STRING') else os.getenv('DB_CONNECTION_STRING')
+def _build_database_url():
+    connection_string = os.getenv('DB_CONNECTION_STRING', '').strip()
+    if connection_string:
+        return connection_string
 
-engine = create_engine(DATABASE_URL, echo=True)
+    return ('postgresql://' + os.getenv('DB_USERNAME') + ':' + os.getenv('DB_PASSWORD') +
+            '@' + os.getenv('DB_HOST') + ':' + os.getenv('DB_PORT') + '/' + os.getenv('DB_DATABASE'))
+
+
+DATABASE_URL = _build_database_url()
+_engine_options = {'echo': True}
+if DATABASE_URL.startswith('sqlite'):
+    _engine_options['connect_args'] = {'check_same_thread': False}
+
+engine = create_engine(DATABASE_URL, **_engine_options)
 
 SessionLocal = sessionmaker(class_=Session, bind=engine, autocommit=False, autoflush=False)
 
